37e9bff8744ad6404637d8e7b2d5e0d5afe5dec3
sagarboyal <sboyal12@gmail.com>
2025-09-06 22:33:01 +0530

    [backend]: app created using spring boot

---
+++ BackEnd/quickgig/src/test/java/com/main/quickgig/QuickgigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.quickgig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickgigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
